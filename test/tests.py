880e64af0fd01601608b360085fd06a17fa950b7
Swarnabh Kashyap <swarnabh202@gmail.com>
2022-07-25 22:47:06 -0400

    Added the implementation of the Fizzbuzz program in python

---
+++ test/tests.py
@@ -0,0 +1,20 @@
+import sys
+import unittest
+
+# adding fizzbuzz to the system path
+sys.path.append("c:\\Users\\swarn\\OneDrive\\Desktop\\Interview_prep2\\FizzBuzz\\FizzBuzz\\src")
+
+from fizzbuzz import fizzbuzz
+class FizzBuzzTests(unittest.TestCase):
+  def test_1(self):
+    self.assertEqual(fizzbuzz(1),[1])
+  def test_3(self):
+    self.assertEqual(fizzbuzz(3),[1,2,'Fizz'])
+  def test_5(self):
+    self.assertEqual(fizzbuzz(5),[1,2,'Fizz',4,'Buzz'])
+  def test_15(self):
+    self.assertEqual(fizzbuzz(15),[1,2,'Fizz',4,'Buzz','Fizz',7,8,'Fizz','Buzz',11,'Fizz',13,14,'FizzBuzz'])
+
+
+if __name__ == '__main__':
+    unittest.main()
